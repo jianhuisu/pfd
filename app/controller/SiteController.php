f112cbb2878a51c4405b5a658bed2d9be1655ea6
sujianhui <1051034413@qq.com>
2019-04-05 12:52:05 +0800

    test git file permision

--- app/controller/SiteController.php
+++ app/controller/SiteController.php
@@ -8,11 +8,13 @@ namespace app\controller;
 
 use vendor\base\Event;
 use vendor\db\db_mysqli;
+use vendor\Log;
 
 class SiteController extends BaseController
 {
     public function actionIndex()
     {
+        Log::set("can I set log ? ");
        echo $this->render('index',['list' => ['sujianhui','zhaojianwei','zhangwenyuan']]);
     }
 
