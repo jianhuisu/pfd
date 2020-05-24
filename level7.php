3f71e8031047a81987700843f31608ffd15ab277
sujianhui <1051034413@qq.com>
2020-05-24 11:09:48 +0800

    add

--- level7.php
+++ level7.php
@@ -1,27 +1,4 @@
 <?php
 
-class ParentClass
-{
-    const MODULE_ID = 0;
-
-    public function getModuleId_byStatic()
-    {
-        return static::MODULE_ID;
-    }
-
-    public function getModuleId_bySelf()
-    {
-        return self::MODULE_ID;
-    }
-
-}
-
-class ChildClass extends ParentClass
-{
-    const MODULE_ID = 1; // 重写父类中的常量
-}
-
-$obj = new ChildClass();
-
-echo $obj->getModuleId_byStatic()."\n";
-echo $obj->getModuleId_bySelf()."\n";
+$x="display";
+${$x.'_result'} ();
