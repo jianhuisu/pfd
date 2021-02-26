c708b62c7b04fd23b11455e242de7291aa960ec7
jianhui.su <jianhui.su@vhall.com>
2021-02-26 16:27:13 +0800

    update

---
+++ documents/php7_usage/解决跨域options请求.md
@@ -0,0 +1,5 @@
+
+1 header('Access-Control-Allow-Headers: request-id,token,platform,interact-token,gray-id,*');
+2 if(method == OPTIONS ){
+    return 200;
+}
--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
-
+//
+//
+//function checkImageUrlIsAvaiable($url)
+//{
+//    $url = "http://local.pfd.com/2.png";
+//    $url = "https://sfs-public.shangdejigou.cn/sunlands_back_freestudy/gw_banner.png";
+//
+//
+//    if(function_exists('get_headers')){
+//
+//        if( $headers = get_headers($url,1) ){
+//
+//            if( !preg_match('/200/',$headers[0]) ){
+//                echo 1;exit;
+//            }
+//            echo 2;exit;
+//        }
+//
+//    }
+//
+//}
+//
+//
+//
+//checkImageUrlIsAvaiable('a');
 
 phpinfo();
 exit;
